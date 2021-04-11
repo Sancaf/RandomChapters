9c0841c295290fddc6c0ffd6828fdb56af9eff99
Santiago Cafferata <shancaf91@gmail.com>
2021-04-11 00:00:24 -0500

    ahora sí anda. Solo queda ponerlo bonito

--- script/index.js
+++ script/index.js
@@ -1,14 +1,20 @@
 const number = document.getElementById('number');
 const button = document.getElementById('button');
 
-let random = () => {
-    let randomNumber = Math.ceil(Math.random() * 13);
-    number.innerText = randomNumber;
-}
-let suspense = () => {
-    let interval = setInterval(random, 113);
-    interval;
-    setTimeout(clearInterval(interval), 3000);
+function btn () {
+
+    let random = () => {
+        let randomNumber = Math.ceil(Math.random() * 13);
+        if (number.innerText && number.innerText == randomNumber) {
+            random();
+        } else {
+            number.innerText = randomNumber;
+        }
+    }
+    
+    let interval = setInterval(random, 127);
+    let stop = () => clearInterval(interval);
+    setTimeout(stop, 5000);
 }
 
-button.addEventListener('click', suspense);
+button.addEventListener('click', btn);
