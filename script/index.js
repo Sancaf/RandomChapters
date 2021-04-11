9ee1c9345efead6d0a61d0b6905894e70b431803
Santiago Cafferata <shancaf91@gmail.com>
2021-04-11 14:18:34 -0500

    simplemente, muy bello

--- script/index.js
+++ script/index.js
@@ -2,19 +2,26 @@ const number = document.getElementById('number');
 const button = document.getElementById('button');
 
 function btn () {
+    button.style.color = "blanchedalmond";
+    button.style.borderColor = "blanchedalmond";
+    button.style.backgroundColor = "black";
 
     let random = () => {
 
         let randomNumber = Math.ceil(Math.random() * 13);
         if (number.innerText && number.innerText == randomNumber) {
             random();
+        } else if (randomNumber <= 9) {
+            number.innerText = '0' + randomNumber;
         } else {
             number.innerText = randomNumber;
         }
     }
     
     let interval = setInterval(random, 127);
-    let stop = () => clearInterval(interval);
+    let stop = () => {
+        clearInterval(interval);
+    }
     setTimeout(stop, 5000);
 }
 
